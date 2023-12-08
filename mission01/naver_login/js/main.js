5820cb0fedff135cb98dbddd2abe3fea00d126cc
lanuioe <lanuioe@gmail.com>
2023-12-09 06:23:10 +0900

    정규표현식 조건처리, 페이지 이동 완료

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,13 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
+
+// 변수
+const input_id = document.querySelector(".user-email-input");
+const input_pw = document.querySelector(".user-password-input");
+const btn_login = document.querySelector(".btn-login");
+const login_error = document.querySelector('.error-message-login')
 
 /*
 
@@ -13,13 +18,56 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// 정규표현식 validation
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
-}
-
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
+
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+
+  return re.test(String(text).toLowerCase());
+}
+
+function handleInputId(e) {
+  const value = e.target.value;
+  // console.log(value);
+  
+  if (emailReg(value) || value === '') {
+    input_id.classList.remove("is--invalid");
+  } else {
+    input_id.classList.add("is--invalid");
+    login_error.classList.remove("is--invalid");
+  }
+}
+
+function handleInputPw(e) {
+  const value = e.target.value;
+  // console.log(value);
+  
+  if (pwReg(value) || value === '') {
+    input_pw.classList.remove("is--invalid");
+  } else {
+    input_pw.classList.add("is--invalid");
+    login_error.classList.remove("is--invalid");
+  }
+}
+
+function movePage(e) {
+  e.preventDefault();
+  
+  if (input_id.value === user.id && input_pw.value === user.pw) {
+    window.location.href = "welcome.html";
+  } else {
+    input_id.classList.remove("is--invalid");
+    input_pw.classList.remove("is--invalid");
+    login_error.classList.add("is--invalid");
+  }
+}
+
+input_id.addEventListener("input", handleInputId);
+input_pw.addEventListener("input", handleInputPw);
+btn_login.addEventListener("click", movePage);
