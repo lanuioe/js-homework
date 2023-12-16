a98cbebf60f57f61575ac3bdbb4987d98e520452
lanuioe <lanuioe@gmail.com>
2023-12-16 21:46:32 +0900

    [add] 오디오 제어 함수 추가

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -13,43 +13,50 @@ const navigation = document.querySelector("nav > ul");
 const visualImage = document.querySelector(".visual img");
 const defaultColor = "#000";
 
-// 배경색 변경 함수
-function setBgColor(colorA, colorB = defaultColor) {
-  document.body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
-}
-
-// 이미지 변경 함수
-function setImg(index) {
-  const { name, alt } = data[index];
-  visualImage.src = `./assets/${name.toLowerCase()}.jpeg`;
-  visualImage.alt = alt;
-}
-
-// 이름 변경 함수
-function setNameText(index) {
-  document.querySelector(".nickName").textContent = data[index].name;
-}
-
 // is-active 설정 함수
 function setActive(li) {
   [...navigation.children].forEach((li) => li.classList.remove("is-active"));
   li.classList.add("is-active");
 }
 
+// 배경색 변경 함수
+function setBgColor(colorA, colorB = defaultColor) {
+  document.body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
+}
+
+// 이미지 변경 함수
+function setImg(name, alt) {
+  visualImage.src = `./assets/${name.toLowerCase()}.jpeg`;
+  visualImage.alt = alt;
+}
+
+// 이름 변경 함수
+function setNameText(name) {
+  document.querySelector(".nickName").textContent = name;
+}
+
+// 오디오 제어 함수
+function loadAudio(name) {
+  const audio = new Audio(`./assets/audio/${name}.m4a`);
+  audio.play();
+}
+
 // click 이벤트 함수
 function handleClick(e) {
   e.preventDefault();
 
-  let li = e.target.closest("li");
+  const li = e.target.closest("li");
   if (!li) return;
 
-  let index = li.dataset.index - 1;
+  const index = li.dataset.index - 1;
   const [colorA, colorB] = data[index].color;
+  const { name, alt } = data[index];
 
   setActive(li);
   setBgColor(colorA, colorB);
-  setImg(index);
-  setNameText(index);
+  setImg(name, alt);
+  setNameText(name);
+  loadAudio(name);
 }
 
 navigation.addEventListener("click", handleClick);
