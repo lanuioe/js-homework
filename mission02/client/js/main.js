abd4950309a8ab98921e8d821f5ac907ffcc165f
lanuioe <lanuioe@gmail.com>
2023-12-16 21:18:15 +0900

    [add] 클릭 이벤트 추가 - 배경색, 이미지, 텍스트 변경

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,48 @@
 
 */
 
+// 변수설정
+const navigation = document.querySelector("nav > ul");
+const visualImage = document.querySelector(".visual img");
+const defaultColor = "#000";
 
+// 배경색 변경 함수
+function setBgColor(colorA, colorB = defaultColor) {
+  document.body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
+}
 
+// 이미지 변경 함수
+function setImg(index) {
+  const { name, alt } = data[index];
+  visualImage.src = `./assets/${name.toLowerCase()}.jpeg`;
+  visualImage.alt = alt;
+}
 
+// 이름 변경 함수
+function setNameText(index) {
+  document.querySelector(".nickName").textContent = data[index].name;
+}
 
+// is-active 설정 함수
+function setActive(li) {
+  [...navigation.children].forEach((li) => li.classList.remove("is-active"));
+  li.classList.add("is-active");
+}
 
+// click 이벤트 함수
+function handleClick(e) {
+  e.preventDefault();
 
+  let li = e.target.closest("li");
+  if (!li) return;
 
+  let index = li.dataset.index - 1;
+  const [colorA, colorB] = data[index].color;
 
+  setActive(li);
+  setBgColor(colorA, colorB);
+  setImg(index);
+  setNameText(index);
+}
 
-
-
-
-
+navigation.addEventListener("click", handleClick);
